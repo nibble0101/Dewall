e77acaf48346e6eefb0ded509f2a8b802e1c2e23
nibble0101 <mj.mawa@yahoo.com>
2022-04-04 19:29:26 +0300

    refactor: remove redundant if statements with querySelectorAll

--- src/content-script.js
+++ src/content-script.js
@@ -8,45 +8,36 @@ if (modal) {
 }
 
 const modalBackdrop = document.querySelectorAll('.modal-backdrop.fade.in');
-if (modalBackdrop) {
-    modalBackdrop.forEach(modalBackdropElement => {
-        modalBackdropElement.remove();
-    })
-}
+modalBackdrop.forEach(modalBackdropElement => {
+    modalBackdropElement.remove();
+});
+
 
 const contentPageAdWrapDesktop = document.querySelectorAll('.content-page-ad_wrap.desktop');
-if (contentPageAdWrapDesktop) {
-    contentPageAdWrapDesktop.forEach(ad => {
-        ad.remove();
-    })
-}
+contentPageAdWrapDesktop.forEach(ad => {
+    ad.remove();
+});
+
 
 const contentPageAdWrapMobile = document.querySelectorAll('.content-page-ad_wrap.mobile');
-if (contentPageAdWrapMobile) {
-    contentPageAdWrapMobile.forEach(mobileContentPageAdWrapper => {
-        mobileContentPageAdWrapper.remove();
-    })
-}
+contentPageAdWrapMobile.forEach(mobileContentPageAdWrapper => {
+    mobileContentPageAdWrapper.remove();
+});
+
 
 const adMobileLeaderBoardWrap = document.querySelectorAll('.ad-mobile-leaderboard_wrap');
-if (adMobileLeaderBoardWrap) {
-    adMobileLeaderBoardWrap.forEach(mobileAdWrapper => {
-        mobileAdWrapper.remove();
-    })
-}
+adMobileLeaderBoardWrap.forEach(mobileAdWrapper => {
+    mobileAdWrapper.remove();
+});
+
 
 const nestedHtml = document.querySelectorAll('body * html');
-if (nestedHtml) {
-    nestedHtml.forEach(html => {
-        html.remove();
-    });
-}
+nestedHtml.forEach(html => {
+    html.remove();
+});
 
 const iframes = document.querySelectorAll('iframe');
-if (iframes) {
-    iframes.forEach(iframe => {
-        iframe.remove();
-    })
-
-}
+iframes.forEach(iframe => {
+    iframe.remove();
+});
 
